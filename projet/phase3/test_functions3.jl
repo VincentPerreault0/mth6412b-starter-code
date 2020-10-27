1ca7e4b59c6eb80ee32cf6482f29279dca1fd368
Laura KOLCHEVA <laura.kolcheva@polymtl.ca>
2020-10-27 19:15:08 +0200

    test function: add nodes, edges and priority queue

--- projet/phase3/test_functions3.jl
+++ projet/phase3/test_functions3.jl
@@ -1,17 +1,26 @@
 include("priority_queue.jl")
 include("../phase2/graph.jl")
 
-"""Fichier de test des fonctions de priority_queue et edge.jl"""
+"""Fichier de test des fonctions de priority_queue et edge.jl et node.jl"""
 function main()
     println("test")
     node1 = Node("Joe", 3.14, 1)
+    node1.minweight=10
     node2 = Node("Steve", exp(1),node1)
+    node2.minweight=2
     node3 = Node("Jill", 4.12)
     edge1 = Edge(500, (node1, node2))
     edge2 = Edge(1000, (node2, node3))
     println(edge1==edge2)
     println(edge1<edge2)
+    println(node1==node2)
+    println(node1<node2)
     q=PriorityQueue([edge1, edge2])
+    tmp=popfirst!(q)
+    show(tmp)
+    tmp2=popfirst!(q)
+    show(tmp2)
+    q=PriorityQueue([node1, node2])
     tmp=popfirst!(q)
     show(tmp)
     tmp2=popfirst!(q)
