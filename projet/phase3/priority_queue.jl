ab7f24631ba1052a89b0ebfec354dafa7130ac0f
Laura KOLCHEVA <laura.kolcheva@polymtl.ca>
2020-10-29 16:22:13 +0200

    fonction add_item et minimum_item. Update popfirst

--- projet/phase3/priority_queue.jl
+++ projet/phase3/priority_queue.jl
@@ -1,6 +1,4 @@
-
 include("../phase1/edge.jl")
-import Base.minimum
 import Base.popfirst!
 
 """ type abstrait de file de priorite"""
@@ -37,31 +35,49 @@ function contains_item(q::PriorityQueue{T}, item::T) where T
 end
 
 """Ajoute `item` à la fin de la file."""
-function push!(q::AbstractQueue{T}, item::T) where T
+function add_item!(q::AbstractQueue{T}, item::T) where T
+    if contains_item(q,item)
+        return(q)
+    end
     push!(q.items, item)
     q
 end
 
-"""renvoie le plus petit element de la file
-attention les fonctions isless et == doivent etre definies pour le type d objet de items"""
-function minimum(q::AbstractQueue)
-    return(minimum(items(q)))
+"""renvoie le plus petit element de la file"""
+function minimum_item(q::AbstractQueue, type ::Type )
+    min=items(q)[1]
+    if type==Node
+        for node in q.items[2:end]
+            if minweight(node)<minweight(min)
+                min=node
+            end
+        end
+    elseif type==Edge
+        for edge in q.items[2:end]
+            if weight(edge)<weight(min)
+                min=node
+            end
+        end
+    end
+    return(min)
 end
 
 """Retire et renvoie un élément ayant la plus haute priorité.
-Ici la priorite est l ordre decroissant. 
-Attention: les fonctions isless et == doivent etre definies pour le type d objet de items"""
-function popfirst!(q::PriorityQueue)
-    highest = minimum(items(q))
+Ici la priorite est l ordre decroissant. """
+function popfirst!(q::PriorityQueue, type::Type)
+    highest = minimum_item(q,type)
     idx = findall(x -> x == highest, q.items)[1]
     deleteat!(q.items, idx)
     return(highest)
 end
 
-"""Retire et renvoie l'élément ayant la plus haute priorité, et qui contient le noeud concerne et son parent."""
+"""Retire et renvoie l'élément ayant la plus haute priorité
+ et qui contient le noeud choisit et son parent.
+ Si il y a plusieurs edge de meme poids, on choisit celui correspondant
+ au noeud choisit."""
 function popfirst!(q:: PriorityQueue{Edge}, node:: Node)
-    highest = minimum(items(q))
-    idx = findall(x -> x == highest, q.items)
+    min= minimum_item(q,Edge)
+    idx = findall(x -> weight(x) == weight(min), q.items)
     tmp=-1
     for i in idx
         if node in nodes(items(q)[i]) && parent(node) in nodes(items(q)[i])
@@ -72,7 +88,7 @@ function popfirst!(q:: PriorityQueue{Edge}, node:: Node)
     if tmp==-1
         println("il y a un probleme avec popfirst!")
     end 
-    tmp2=items(q)[tmp]
+    edge=items(q)[tmp]
     deleteat!(q.items, tmp)
-    return(tmp2)
+    return(edge)
 end
