5bb41138efec8efa768ea3f0b2644eb6989583ac
Laura KOLCHEVA <laura.kolcheva@polymtl.ca>
2020-10-29 16:21:31 +0200

    update des boucles et de fonctions

--- projet/phase3/prim.jl
+++ projet/phase3/prim.jl
@@ -4,20 +4,20 @@ include("priority_queue.jl")
 """" fonction qui prend un graphe et une source et renvoie
 un minimal spanning tree par l algorithme de Prim"""
 
-function Prim(graph :: AbstractGraph, s :: AbstractNode)
-    if not s in nodes(graph)
-        println("la source doit etre un noeud du graphe")
+function prim(graph :: AbstractGraph, s :: AbstractNode)
+    if (s in nodes(graph))==false
         return
     end
+
     #initialisation de la liste d'arretes de l arbre
     new_edges=Vector{Edge}()
 
     #Initialisation du dictionnaire des noeuds et listes d'adjacence
-    for node in nodes(graph)
-        node.weight=10000
-        node.parent=nothing
-        node.neighbours=Node{typeof(data(node))}[]
-    end
+    #for node in nodes(graph)
+    #    node.minweight=10000
+    #    node.parent=nothing
+    #    node.neighbours=Node{typeof(data(node))}[]
+    #end
 
     # Calcul des listes d adjacence
     for edge in edges(graph)
@@ -28,7 +28,7 @@ function Prim(graph :: AbstractGraph, s :: AbstractNode)
     end 
 
     #initialisation 
-    s.weight=0
+    s.minweight=0
     q=PriorityQueue{Node}()
     for node in nodes(graph)
         push!(q, node)
@@ -40,33 +40,25 @@ function Prim(graph :: AbstractGraph, s :: AbstractNode)
     #Boucle
     while nb_items(q)>0
         #on sort un noeud de minweight minimal
-        u=popfirst!(q)
+        u=popfirst!(q, Node)
 
-        #on ajoute l arrete concernee aux arretes du minimum spanning tree
-        egde_tmp=popfirst!(p)
-        if u in nodes(edge_tmp) && parent(u) in nodes(edges_tmp)
-            p_tmp=priorityQueue{Edge}(edge_tmp)
-            while not (u in nodes(edge_tmp) && parent(u) in nodes(edges_tmp))
-                edge_tmp=popfirst!(p)
-                push!(p_tmp, edge_tmp)
-            end
-            for edge in p_tmp
-                push!(p, edge)
-            end
-        end
+        #on ajoute l arrete correspondante aux arretes du minimum spanning tree
+        egde_tmp=popfirst!(p,u)
         push!(new_edges, edge_tmp)
 
         #on actualise les valeurs des noeuds voisins 
         for v in neigbours(u)
-            for edge in edges(graph)
-                if u in edges(node) && v in edges(node) && weight(edge)<minweight(v)
-                    v.parent=u
-                    v.minweight=weight(edge)
-                    push!(p, edge)
+            if contains_item(q,v)
+                for edge in edges(graph)
+                    if u in edges(node) && v in edges(node) && weight(edge)<minweight(v)
+                        v.parent=u
+                        v.minweight=weight(edge)
+                        push!(p, edge)
+                    end
+                    break
                 end
-                break
             end
         end
     end
-    g=Graph("Minimum Spanning tree of"*graph(name), nodes(graph), new_edges)
+    g=Graph("Minimum Spanning tree from Prim alg of"*graph(name), nodes(graph), new_edges)
 end
