20fc1bc8a47565776e065cc60545122e117ea90d
LauraKolcheva <laura.kolcheva@polymtl.ca>
2020-12-04 19:22:25 +0200

    on peut choisir entre RSL et HK pour l'algo

--- projet/phase5/unshredding.jl
+++ projet/phase5/unshredding.jl
@@ -9,38 +9,51 @@ function tsp_cost(tour::AbstractGraph)
     return(cost)
 end 
 
-function unshred(graph::AbstractGraph)
-    #Step 1: Find minimal tour using Held and Karp 
-    pi_mg = zeros(nb_nodes(graph))
-    tree_graph, max_wk = max_w_lk(graph, 1.0, 1000, pi_mg, true, false)
-    graphe_tour = get_tour(graph, tree_graph)
-    if is_tour(graphe_tour)
+function unshred(graph::AbstractGraph, hk::Bool, view::Bool)
+    if hk #Use Held and Karp alg
+        #Step 1: Find minimal tour
+        pi_mg = zeros(nb_nodes(graph))
+        tree_graph, max_wk = max_w_lk(graph, 1.0, 200, pi_mg, true, false)
+        graphe_tour = get_tour(graph, tree_graph)
+        if is_tour(graphe_tour)
         println("tour complete")
-    else
-        println("Not a tour")
-    end
+        else
+            println("Not a tour")
+        end
+        #Step 2: Create an array with the order
+        liste=Vector{Int64}()
+        for edge in edges(graphe_tour)
+            push!(liste, data(nodes(edge)[1]))
+        end
+        #Step 3: Find cost of tour
+        cost=tsp_cost(graphe_tour)
 
-    #Step 2: Create an array with the order
-    liste=Vector{Int64}()
-    for edge in edges(graphe_tour)
-        push!(liste, data(nodes(edge)[1]))
+    else #use RSL
+        #Step1: Find minimal tour 
+        tmp=rsl(graph, nodes(graph)[1])
+        #Step2: Create an array with the order
+        liste=Vector{Int64}()
+        for node in tmp
+            push!(liste, data(node))
+        end 
+        #Step 3: Find cost of tour
+        cost=rsl_graph_weight(graph, tmp)
     end 
 
-    #Step3: Write tour
-    cost=tsp_cost(graphe_tour)
+    #Step 4: Write tour
     tour_name=name(graph)*" tour"
     write_tour(tour_name,liste, cost)
 
-    #Step4: Reconstruct picture
+    #Step 5: Reconstruct picture
     filename=name(graph)
-    pic=reconstruct_picture(tour_name, filename, "reconstructed "*filename)
+    reconstruct_picture(tour_name, filename, "reconstructed "*filename, view)
     return(pic)
 end
 
-function unshred(filename::String)
+function unshred(filename::String, hk::Bool, view::Bool)
     #Step 1: Create graph 
     graph = create_graph_from_stsp_file(filename, false)
     show(graph)
     #Step2: reconstruct
-    return(unshred(graph))
+    return(unshred(graph,hk,view))
 end 
