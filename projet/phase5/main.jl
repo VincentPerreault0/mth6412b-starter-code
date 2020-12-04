64dfbb3671f95161cf3ef0496d092f936934ebda
LauraKolcheva <laura.kolcheva@polymtl.ca>
2020-12-04 15:38:53 +0200

    travail sur la fonction unshredding

--- projet/phase5/main.jl
+++ projet/phase5/main.jl
@@ -1,17 +1,15 @@
 include("unshredding.jl")
 
-filenames = ["instances/stsp/bayg29.tsp",
-            "instances/stsp/bays29.tsp",
-            "instances/stsp/brazil58.tsp",
-            "instances/stsp/dantzig42.tsp",
-            "instances/stsp/fri26.tsp",
-            "instances/stsp/gr17.tsp",
-            "instances/stsp/gr21.tsp",
-            "instances/stsp/gr24.tsp",
-            "instances/stsp/gr48.tsp",
-            "instances/stsp/hk48.tsp",
-            "instances/stsp/swiss42.tsp"]
-
+filenames = ["projet/phase5/tsp/instances/abstract-light-painting.tsp",
+            "projet/phase5/tsp/instances/alaska-railroad.tsp",
+            "projet/phase5/tsp/instances/blue-hour-paris.tsp",
+            "projet/phase5/tsp/instances/lower-kananaskis-lake.tsp",
+            "projet/phase5/tsp/instances/marlet2-radio-board.tsp",
+            "projet/phase5/tsp/instances/nikos-cat.tsp",
+            "projet/phase5/tsp/instances/pizza-food-wallpaper.tsp",
+            "projet/phase5/tsp/instances/the-enchanted-garden.tsp",
+            "projet/phase5/tsp/instances/tokyo-skytree-aerial.tsp"]
 for filename in filenames
+    println("on commence l'image", filename)
     unshred(filename)
 end 
--- projet/phase5/unshredding.jl
+++ projet/phase5/unshredding.jl
@@ -4,11 +4,22 @@ include("tools.jl")
 function unshred(filename::String)
     #Step 1: Create graph 
     graph = create_graph_from_stsp_file(filename, false)
-
     #Step 2: Find minimal tour using Held and Karp 
     pi_mg = zeros(nb_nodes(graph))
     tree_graph, max_wk = max_w_lk(graph, 1.0, 1000, pi_mg, true, false)
-    tour_graph = get_tour(graph, tree_graph)
-    show(tour_graph)
+    tour = get_tour(graph, tree_graph)
+    if is_tour(tour)
+        println("Test réussi")
+        show(tour)
+    else
+        println("not a tour")
+    end
+    #Step 3: Create an array with the order
+    liste=Array{Int64}()
+    for edge in edges(tour)
+        push!(liste, data(nodes(edge)[1]))
+    end 
+    
+
 end
 
